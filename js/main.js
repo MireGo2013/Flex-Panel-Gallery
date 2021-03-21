113fb654f60f46fbf1b553f0ad150fbdd8ece108
MireGo2013 <sobol.v92@gmail.com>
2021-03-21 12:08:38 +0300

    style: change to e.target

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
-const panels = document.querySelectorAll('.wrapper_panels .panel');
 
+const panelse = document.querySelector('.wrapper_panels');
 
-function toggleOpen() {
-	panels.forEach(panel => panel.classList.remove('open'));
-	this.classList.add('open');
-}
-
-
-function toggleActive(e) {
-	console.log(e.propertyName);
-	if (e.propertyName.includes('flex')) {
-		this.classList.toggle('active');
+function toggleOpen(e) {
+	if (e.target.classList.contains('open')) {
+		e.target.classList.remove('open')
+	} else {
+		document.querySelector('.open')?.classList.remove('open');
+		e.target.classList.add('open');
 	}
 }
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+function toggleActive(e) {
+	if (e.propertyName.includes('flex')) {
+		e.target.classList.toggle('active');
+	}
+}
+
+panelse.addEventListener('click', function (e) {
+	if (e.target.closest('.panel')) {
+		toggleOpen(e);
+	}
+})
+
+document.body.addEventListener('transitionend', function (e) {
+	toggleActive(e)
+})
+
 
